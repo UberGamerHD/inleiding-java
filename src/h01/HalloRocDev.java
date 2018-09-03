b8015996e63fb597c570b4ecd4f9f6d8fbdff129
Mert Ari <Mertarie10@gmail.com>
2018-09-03 13:10:20 +0200

    Text change

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo dit is mert ari!! :)");
+        System.out.println("Hallo Mert vam klas 8A! hier");
     }
 }
