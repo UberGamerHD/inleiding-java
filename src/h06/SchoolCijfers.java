016ba54f4c1abe7a32958e154877f266337f33b7
Mert Ari <Mertarie10@gmail.com>
2018-09-12 13:14:48 +0200

    Alle opdrachen tot en met hoofdstuk 6 af.

--- src/h06/SchoolCijfers.java
+++ src/h06/SchoolCijfers.java
@@ -11,6 +11,8 @@ public class SchoolCijfers extends Applet {
     int e;
     double uitkomst;
     double uitkomst2;
+    int geheel;
+    double gebroken;
 
     public void init() {
         a = 5.9;
@@ -19,10 +21,13 @@ public class SchoolCijfers extends Applet {
         d = 3;
         e = 10;
         uitkomst = (a + b + c) / d;
-        uitkomst2 = uitkomst * e;
+        uitkomst2 = uitkomst * 10;
+        uitkomst2 = (int) uitkomst2;
+        uitkomst2 = (uitkomst2 / e);
+
     }
     public void paint(Graphics g) {
-        g.drawString("Hey Gemiddelde: " + uitkomst , 20 , 20);
+        g.drawString("Het Gemiddelde: " + uitkomst2 , 20 , 20);
 
     }
 }
